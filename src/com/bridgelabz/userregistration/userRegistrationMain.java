6764708df82d4981f1a82b5008e3a0725ea66134
kaushalkumarroy <kaushal.dakshana16@gmail.com>
2020-10-28 22:21:27 +0530

    [Kaushal Kumar]Added : validating regex for Last Name

--- src/com/bridgelabz/userregistration/userRegistrationMain.java
+++ src/com/bridgelabz/userregistration/userRegistrationMain.java
@@ -16,5 +16,24 @@ public class userRegistrationMain {
 		else {
 			System.out.println("Invalid name.");
 		}
+		
+		//UC2
+		System.out.println("Enter Last Name:");
+		String lastName = consoleInputReader.next();
+		String validLastName = "^[A-Z][a-zA-Z]+{2}";
+		boolean result1  = Pattern.matches(validFirstName, name);
+		if(result1) {
+			System.out.println("Valid name");
+		}
+		else {
+			System.out.println("Invalid name.");
+		}
+		
+		if(result && result1){
+			System.out.println("User's Name is: " + name + " " + lastName);
+		}
+		else {
+			System.out.println("Either first name or last name is invalid");
+		}
 	}
 }
