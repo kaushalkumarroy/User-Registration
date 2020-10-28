5218571cdf511a4aec0aa9758e968c8a6b18f448
kaushalkumarroy <kaushal.dakshana16@gmail.com>
2020-10-28 20:11:37 +0530

    [Kaushal Kumar]Added : Regex validating the First Name

---
+++ src/com/bridgelabz/userregistration/userRegistrationMain.java
@@ -0,0 +1,17 @@
+package com.bridgelabz.userregistration;
+
+import java.util.*;
+import java.util.regex.Pattern;
+public class userRegistrationMain {
+	public static void main() {
+		Scanner consoleInputReader = new Scanner(System.in);
+		//UC1
+		System.out.println("Enter First Name:");
+		String name = consoleInputReader.next();
+		String validFirstName = "^[A-Z] {3,}";
+		boolean result  = Pattern.matches(validFirstName, name);
+		if(result) {
+			System.out.println("Valid name");
+		}
+	}
+}
